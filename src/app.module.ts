25db80e523c3d2b31277c5b6b251bfe222a593df
zqlimy17 <zqlimy@gmail.com>
2021-09-08 10:19:31 +0800

    created dto

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { DatabaseModule } from './database/database.module';
+import { CatsController } from './cats/cats.controller';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
+  imports: [DatabaseModule],
+  controllers: [AppController, CatsController],
   providers: [AppService],
 })
 export class AppModule {}
